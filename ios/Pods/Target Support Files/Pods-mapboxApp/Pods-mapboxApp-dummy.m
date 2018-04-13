05d3e7338fc2a497bf9edaa1a7d976c40a9c87a4
blue-sky0909 <daiki0115@yandex.com>
2018-04-13 18:58:49 +0200

    show mapbox

---
+++ ios/Pods/Target Support Files/Pods-mapboxApp/Pods-mapboxApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mapboxApp : NSObject
+@end
+@implementation PodsDummy_Pods_mapboxApp
+@end
---
+++ ios/Pods/Target Support Files/Pods-mapboxAppTests/Pods-mapboxAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mapboxAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_mapboxAppTests
+@end
